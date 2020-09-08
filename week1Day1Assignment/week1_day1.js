85f7b060d73d085008bf367cae2b12f8728ff3d3
biniyy149 <biniyamyemane@yahoo.com>
2020-09-07 19:02:57 -0700

    Update week1_day1.js

--- week1Day1Assignment/week1_day1.js
+++ week1Day1Assignment/week1_day1.js
@@ -8,3 +8,26 @@ function checkAge(age) {
   }
   //else is not required for the above code
   //we can just return confirm outside the if clause.
+
+  //i will do the function using tertiary notation.
+  function checkAge(age){
+    return (age > 18)? ture:confirm('did parents allow you?');
+  }
+  //the minimum of the two numbers
+  function min(a,b){
+    if(a<b){
+      return a;
+    }else{
+      return b;
+    }
+  }
+  //the power of a number
+  function pow(x, y) {
+    let initial = x;
+  
+    for (let i = 1; i < y; i++) {
+      initial *= x;
+    }
+  
+    return initial;
+  }
