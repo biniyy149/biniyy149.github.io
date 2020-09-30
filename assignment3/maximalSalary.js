43b64a239127a3c344ba15be53391d71981d4b6c
biniyy149 <biniyamyemane@yahoo.com>
2020-09-29 18:50:58 -0700

    today's assignment

---
+++ assignment3/maximalSalary.js
@@ -0,0 +1,29 @@
+{
+function topSalary(salaries) {
+    let max = 0;
+    let maxName = null;
+    for(const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+    max = salary;
+    maxName = name;
+    }
+    }
+    return maxName;
+    }
+    describe("topSalary", function() {
+        it("returns top-paid person", function() {
+          let salaries = {
+            "John": 100,
+            "Pete": 300,
+            "Mary": 250
+          };
+      
+          assert.equal( topSalary(salaries), "Pete" );
+        });
+      
+        it("returns null for the empty object", function() {
+          assert.isNull( topSalary({}) );
+        });
+      });
+    }
+    
