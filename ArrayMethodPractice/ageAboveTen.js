a6afd737b3b04245f9b42f8ce4173c974b74d31b
biniyy149 <biniyamyemane@yahoo.com>
2020-10-02 07:49:42 -0700

    alerted

--- ArrayMethodPractice/ageAboveTen.js
+++ ArrayMethodPractice/ageAboveTen.js
@@ -6,6 +6,7 @@ function ageAboveTen(arr){
 const peopleArray = [{name: "Sam", age: 15}, {name: "William", age: 6}, {name: "Lucy", age: 13}, {name:
     "Barney", age: 80}];
 console.log(ageAboveTen(peopleArray));
+alert(ageAboveTen(peopleArray));
 describe("ageAboveTen", function() {
 
     it("Ages which are above 10", function() {
